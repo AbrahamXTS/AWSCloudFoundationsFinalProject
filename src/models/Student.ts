e211adcf0723a3f10784a3895b54632e4a0e6004
Abraham Espinosa <abrahamespinosa3132@gmail.com>
2025-11-03 23:06:12 -0600

    feat: :sparkles: define schemas for Student and Teacher using Zod for validation

--- src/models/Student.ts
+++ src/models/Student.ts
@@ -1,7 +1,11 @@
-export interface Student {
-	id: number;
-	nombres: string;
-	apellidos: string;
-	matricula: string;
-	promedio: number;
-}
+import z from "zod";
+
+export const StudentSchema = z.object({
+	id: z.number().int().positive(),
+	nombres: z.string().nonempty(),
+	apellidos: z.string().nonempty(),
+	matricula: z.string().nonempty(),
+	promedio: z.number().min(0).max(100),
+});
+
+export type Student = z.infer<typeof StudentSchema>;
--- src/models/Teacher.ts
+++ src/models/Teacher.ts
@@ -1,3 +1,13 @@
+import z from "zod";
+
+export const TeacherSchema = z.object({
+	id: z.number().int().positive(),
+	numeroEmpleado: z.number().int().positive(),
+	nombres: z.string().nonempty(),
+	apellidos: z.string().nonempty(),
+	horasClase: z.number().min(0),
+});
+
 export interface Teacher {
 	id: number;
 	numeroEmpleado: number;
